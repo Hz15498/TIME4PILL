e39c484a4ebb94f5b2cb25c35c410b90c4d6bb4b
davidarnedo10 <davidarnedolozano@gmail.com>
2025-05-16 11:56:11 +0200

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -1,8 +1,49 @@
 package Time4Pill;
 
-public class Paciente extends Usuario {
-	    public Paciente(String id, String nombre, String email, String contrasena) {
-	        super(id,nombre,email,contrasena);
-	    }
-}
+import java.util.ArrayList;
+import java.util.List;
+
+class Paciente extends Usuario {
+	    private List<Medicamento> medicamentos;
+	    private List<Recordatorio> recordatorios;
+	    private Calendario calendario;
+
+	    public Paciente( String nombreUsuario, String email, String contraseña,String tipoUsuario) {
+	        super(nombreUsuario,contraseña,email, tipoUsuario);
+	        medicamentos = new ArrayList<>();
+	        recordatorios = new ArrayList<>();
+	        calendario = new Calendario();
 	    
+	    }
+
+	    public void confirmarToma() {
+	        System.out.println(nombreUsuario + " ha confirmado la toma.");
+	    }
+
+	    public void recibirAlarma() {
+	        System.out.println("Alarma recibida por " + nombreUsuario);
+	    }
+
+	    public void notificarAlarma() {
+	        System.out.println("Alarma notificada al cuidador desde " + nombreUsuario);
+	    }
+
+	    public void compararDosis(Usuario receptor) {
+	        System.out.println(nombreUsuario + " compara dosis con " + receptor.nombreUsuario);
+	    }
+
+	    public Calendario getCalendario() {
+	        return calendario;
+	    }
+
+	    public void agregarRecordatorio(Recordatorio r) {
+	        recordatorios.add(r);
+	    }
+
+	    public void agregarMedicamento(Medicamento m) {
+	        medicamentos.add(m);
+	    }
+
+	   
+	}
+
