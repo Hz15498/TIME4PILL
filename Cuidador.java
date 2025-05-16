f84003d8f98cdd495d4822cff83760730d2d1f83
davidarnedo10 <davidarnedolozano@gmail.com>
2025-05-16 11:59:30 +0200

    Update Cuidador.java

--- Cuidador.java
+++ Cuidador.java
@@ -1,7 +1,28 @@
 package Time4Pill;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cuidador extends Usuario {
-    public Cuidador(String id, String nombre, String email, String contrasena) {
-        super(id,nombre,email,contrasena);
+    private List<Pacientes> pacientesACargo;
+    
+    public Cuidador(String nombreUsuario, String email, String contrasena,String tipoUsuario) {
+        super(nombreUsuario, contrasena, email,tipoUsuario);
+        this.pacientesACargo = new ArrayList<>();
+    }
+    
+    public void agregarPaciente(Pacientes paciente) {
+        pacientesACargo.add(paciente);
+    }
+    
+    public void recibirNotificacion(String mensaje) {
+        System.out.println("Notificación para cuidador " + nombreUsuario + ": " + mensaje);
+    }
+    
+    public void verificarTomasPacientes() {
+        System.out.println("Verificando tomas de pacientes a cargo...");
+        for (Pacientes paciente : pacientesACargo) {
+            System.out.println("Revisando tomas de " + paciente.getNombreUsuario());
+        }
     }
 }
