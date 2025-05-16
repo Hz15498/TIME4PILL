89bda4390f83982afe3b317b420dcc7b483cad00
davidarnedo10 <davidarnedolozano@gmail.com>
2025-05-16 11:57:40 +0200

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,27 +1,54 @@
 package Time4Pill;
 
-public abstract class Usuario {
-    private String id;
-    private String nombre;
-    private String email;
-    private String contrasena;
+import java.io.Serializable;
 
-    public Usuario(String id, String nombre, String email, String contrasena) {
-        this.id = id;
-        this.nombre = nombre;
-        this.email = email;
-        this.contrasena = contrasena;
-    }
+abstract class Usuario implements Serializable {
+	
+		
+		protected String nombreUsuario;
+	    private String contrasena;
+	    private String email;
+	    private String tipoUsuario;
+	    public Usuario(String nombreUsuario, String contrasena, String email,String tipoUsuario) {
+	        this.nombreUsuario = nombreUsuario;
+	        this.contrasena = contrasena;
+	        this.email = email;
+	        this.tipoUsuario=tipoUsuario;
+	    }
+	    
+	    // Getters y Setters
+	    public String getNombreUsuario() {
+	        return nombreUsuario;
+	    }
+	    
+	    public void setnombreUsuario(String nombreUsuario) {
+	        this.nombreUsuario= nombreUsuario;
+	    }
+	    
+	    public String getContrasena() {
+	        return contrasena;
+	    }
+	    
+	    public void setContrasena(String contrasena) {
+	        this.contrasena = contrasena;
+	    }
+	    
+	    public String getEmail() {
+	        return email;
+	    }
+	    
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+	    public String getTipoUsuario() {
+	        return tipoUsuario;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return "Usuario\n Nombre de usuario=" + nombreUsuario + "\n email=" + email + "Tipo de usuario: "+tipoUsuario+"]";
+	    }
+	    
+	}
 
-    public String getId(){ 
-    	return id; 
-    }
-    public String getNombre(){ 
-    	return nombre; 
-    }
-    public String getEmail(){ 
-    	return email; 
-    }
-    public String getContrasena(){ 
-    	return contrasena; }
-}
+
