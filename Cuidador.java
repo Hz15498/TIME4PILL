e9ad9b0fc70c5cad98e5d13f48a21728251d1760
Hz15498 <104327158+Hz15498@users.noreply.github.com>
2025-05-22 05:32:01 -0400

    Update Cuidador.java

--- Cuidador.java
+++ Cuidador.java
@@ -1,28 +1,27 @@
 package Time4Pill;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cuidador extends Usuario {
-    private List<Pacientes> pacientesACargo;
+    private List<Paciente> pacientesACargo;
     
-    public Cuidador(String nombreUsuario, String email, String contrasena,String tipoUsuario) {
-        super(nombreUsuario, contrasena, email,tipoUsuario);
+    public Cuidador(String id, String nombre, String email, String contrasena,String tipoUsuario) {
+        super(id,nombre, email,contrasena, tipoUsuario);
         this.pacientesACargo = new ArrayList<>();
     }
     
-    public void agregarPaciente(Pacientes paciente) {
+    public void agregarPaciente(Paciente paciente) {
         pacientesACargo.add(paciente);
     }
     
     public void recibirNotificacion(String mensaje) {
-        System.out.println("Notificación para cuidador " + nombreUsuario + ": " + mensaje);
+        System.out.println("Notificación para cuidador " + getNombre() + ": " + mensaje);
     }
     
     public void verificarTomasPacientes() {
         System.out.println("Verificando tomas de pacientes a cargo...");
-        for (Pacientes paciente : pacientesACargo) {
-            System.out.println("Revisando tomas de " + paciente.getNombreUsuario());
+        for (Paciente paciente : pacientesACargo) {
+            System.out.println("Revisando tomas de " + paciente.getNombre());
         }
     }
 }
