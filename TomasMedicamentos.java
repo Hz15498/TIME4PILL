33f6e483d824302675f5ceedd7ce2376192931ed
Hz15498 <104327158+Hz15498@users.noreply.github.com>
2025-05-22 05:34:41 -0400

    Update TomasMedicamentos.java

--- TomasMedicamentos.java
+++ TomasMedicamentos.java
@@ -1,11 +1,11 @@
+package Time4Pill;
 import java.sql.Date;
 
-public class TomasMedicamentos extends Tomas {
-	    private Medicamento medicamento;
+public class TomasMedicamentos {
+	 private Medicamento medicamento;
 
 	    public TomasMedicamentos(Medicamento medicamento, Date fechaHora, String descripcion) {
 	        this.medicamento = medicamento;
-	        this.fechaHora = fechaHora;
-	        this.descripcion = descripcion;
+	        super(fechaHora,descripcion);
 	    }
 }
