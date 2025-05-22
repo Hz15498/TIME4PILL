501d8d2ddf1a57006917b00fc2989869e1983141
davidarnedo10 <davidarnedolozano@gmail.com>
2025-05-22 10:38:04 +0200

    Create Tomas.java

---
+++ Tomas.java
@@ -0,0 +1,12 @@
+import java.sql.Date;
+import java.sql.Time;
+
+public class Tomas {
+	    protected Date fechaHora;
+	    protected String descripcion;
+
+	    public String getDescripcion() {
+	        return descripcion;
+	    }
+	}
+
